4f2a5cc8a1819d514e7bba7bf17dadb22f623ea7
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2023-02-05 00:47:46 +0900

    feat create user - 디비에 유저 저장

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -29,23 +29,23 @@ import { AuthRegisterLoginDto } from './dto/auth-register-login.dto';
 export class AuthController {
   constructor(public service: AuthService) {}
 
-  @SerializeOptions({
-    groups: ['me'],
-  })
-  @Post('email/login')
-  @HttpCode(HttpStatus.OK)
-  public async login(@Body() loginDto: AuthEmailLoginDto) {
-    return this.service.validateLogin(loginDto, false);
-  }
+  // @SerializeOptions({
+  //   groups: ['me'],
+  // })
+  // @Post('email/login')
+  // @HttpCode(HttpStatus.OK)
+  // public async login(@Body() loginDto: AuthEmailLoginDto) {
+  //   return this.service.validateLogin(loginDto, false);
+  // }
 
-  @SerializeOptions({
-    groups: ['me'],
-  })
-  @Post('admin/email/login')
-  @HttpCode(HttpStatus.OK)
-  public async adminLogin(@Body() loginDTO: AuthEmailLoginDto) {
-    return this.service.validateLogin(loginDTO, true);
-  }
+  // @SerializeOptions({
+  //   groups: ['me'],
+  // })
+  // @Post('admin/email/login')
+  // @HttpCode(HttpStatus.OK)
+  // public async adminLogin(@Body() loginDTO: AuthEmailLoginDto) {
+  //   return this.service.validateLogin(loginDTO, true);
+  // }
 
   @Post('email/register')
   @HttpCode(HttpStatus.CREATED)
@@ -53,54 +53,54 @@ export class AuthController {
     return this.service.register(createUserDto);
   }
 
-  @Post('email/confirm')
-  @HttpCode(HttpStatus.OK)
-  async confirmEmail(@Body() confirmEmailDto: AuthConfirmEmailDto) {
-    return this.service.confirmEmail(confirmEmailDto.hash);
-  }
+  // @Post('email/confirm')
+  // @HttpCode(HttpStatus.OK)
+  // async confirmEmail(@Body() confirmEmailDto: AuthConfirmEmailDto) {
+  //   return this.service.confirmEmail(confirmEmailDto.hash);
+  // }
 
-  @Post('forgot/password')
-  @HttpCode(HttpStatus.OK)
-  async forgotPassword(@Body() forgotPasswordDto: AuthForgotPasswordDto) {
-    return this.service.forgotPassword(forgotPasswordDto.email);
-  }
+  // @Post('forgot/password')
+  // @HttpCode(HttpStatus.OK)
+  // async forgotPassword(@Body() forgotPasswordDto: AuthForgotPasswordDto) {
+  //   return this.service.forgotPassword(forgotPasswordDto.email);
+  // }
 
-  @Post('reset/password')
-  @HttpCode(HttpStatus.OK)
-  async resetPassword(@Body() resetPasswordDto: AuthResetPasswordDto) {
-    return this.service.resetPassword(
-      resetPasswordDto.hash,
-      resetPasswordDto.password,
-    );
-  }
+  // @Post('reset/password')
+  // @HttpCode(HttpStatus.OK)
+  // async resetPassword(@Body() resetPasswordDto: AuthResetPasswordDto) {
+  //   return this.service.resetPassword(
+  //     resetPasswordDto.hash,
+  //     resetPasswordDto.password,
+  //   );
+  // }
 
-  @ApiBearerAuth()
-  @SerializeOptions({
-    groups: ['me'],
-  })
-  @Get('me')
-  @UseGuards(AuthGuard('jwt'))
-  @HttpCode(HttpStatus.OK)
-  public async me(@Request() request) {
-    return this.service.me(request.user);
-  }
+  // @ApiBearerAuth()
+  // @SerializeOptions({
+  //   groups: ['me'],
+  // })
+  // @Get('me')
+  // @UseGuards(AuthGuard('jwt'))
+  // @HttpCode(HttpStatus.OK)
+  // public async me(@Request() request) {
+  //   return this.service.me(request.user);
+  // }
 
-  @ApiBearerAuth()
-  @SerializeOptions({
-    groups: ['me'],
-  })
-  @Patch('me')
-  @UseGuards(AuthGuard('jwt'))
-  @HttpCode(HttpStatus.OK)
-  public async update(@Request() request, @Body() userDto: AuthUpdateDto) {
-    return this.service.update(request.user, userDto);
-  }
+  // @ApiBearerAuth()
+  // @SerializeOptions({
+  //   groups: ['me'],
+  // })
+  // @Patch('me')
+  // @UseGuards(AuthGuard('jwt'))
+  // @HttpCode(HttpStatus.OK)
+  // public async update(@Request() request, @Body() userDto: AuthUpdateDto) {
+  //   return this.service.update(request.user, userDto);
+  // }
 
-  @ApiBearerAuth()
-  @Delete('me')
-  @UseGuards(AuthGuard('jwt'))
-  @HttpCode(HttpStatus.OK)
-  public async delete(@Request() request) {
-    return this.service.softDelete(request.user);
-  }
+  // @ApiBearerAuth()
+  // @Delete('me')
+  // @UseGuards(AuthGuard('jwt'))
+  // @HttpCode(HttpStatus.OK)
+  // public async delete(@Request() request) {
+  //   return this.service.softDelete(request.user);
+  // }
 }
