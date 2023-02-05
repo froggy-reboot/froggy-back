5131a51eb3160599bcd253ec4053f4e1e3ba0d6c
synodical <lena0323park@naver.com>
2023-02-05 14:34:55 +0900

    Docs : update api property of api docs

--- src/auth/dto/auth-email-login.dto.ts
+++ src/auth/dto/auth-email-login.dto.ts
@@ -4,11 +4,13 @@ import { IsExist } from 'src/utils/validators/is-exists.validator';
 import { Transform } from 'class-transformer';
 
 export class AuthEmailLoginDto {
-  @ApiProperty({ example: 'test1@example.com' })
+  @ApiProperty({ example: 'test@example.com' })
+  // 자동으로 스웨거에 등록하는 데코레이터
+
   @IsNotEmpty()
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: 'hi' })
   @IsNotEmpty()
   password: string;
 }
