f4d16b65a7f64e791748f37410f224ee1f47d19c
synodical <lena0323park@naver.com>
2023-02-05 14:36:06 +0900

    Docs : update api property

--- src/auth/dto/auth-email-login.dto.ts
+++ src/auth/dto/auth-email-login.dto.ts
@@ -6,7 +6,6 @@ import { Transform } from 'class-transformer';
 export class AuthEmailLoginDto {
   @ApiProperty({ example: 'test@example.com' })
   // 자동으로 스웨거에 등록하는 데코레이터
-
   @IsNotEmpty()
   email: string;
 
