1f856b14f993781f42633b8864f2c5058b1a95a0
synodical <lena0323park@naver.com>
2023-02-12 00:10:57 +0900

    "Feat : create naver strategy and config"

--- src/auth-naver/auth-naver.service.ts
+++ src/auth-naver/auth-naver.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { OAuth2Client } from 'google-auth-library';
+import { SocialInterface } from '../social/interfaces/social.interface';
+import { AuthNaverLoginDto } from './dto/auth-naver-login.dto';
 
 @Injectable()
-export class AuthNaverService {}
+export class AuthNaverService {
+  private naver: OAuth2Client;
+
+  constructor(private configService: ConfigService) {
+    this.naver = new OAuth2Client(
+      configService.get('naver.clientId'),
+      configService.get('naver.clientSecret'),
+    );
+  }
+
+  async getProfileByToken(
+    loginDto: AuthNaverLoginDto,
+  ): Promise<SocialInterface> {
+    const ticket = await this.naver.verifyIdToken({
+      idToken: loginDto.idToken,
+      audience: [this.configService.get('naver.clientId')],
+    });
+
+    const data = ticket.getPayload();
+
+    return {
+      id: data.sub,
+      email: data.email,
+    };
+  }
+}
