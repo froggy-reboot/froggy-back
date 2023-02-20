53dc8062c8a8affd17613b4f30f2c2af2d82f876
synodical <lena0323park@naver.com>
2023-02-20 23:02:06 +0900

    FEAT : 네이버 회원가입 기능 추가 (랜덤 닉네임 미생성으로 오류 존재)

--- src/auth-naver/auth-naver.controller.ts
+++ src/auth-naver/auth-naver.controller.ts
@@ -7,13 +7,15 @@ import {
   Post,
   Req,
   Res,
-  UseGuards
-} from "@nestjs/common";
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiResponse, ApiTags } from "@nestjs/swagger";
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from 'src/auth/auth.service';
 import { AuthNaverService } from './auth-naver.service';
 import { AuthNaverLoginDto } from './dto/auth-naver-login.dto';
+import { SocialInterface } from '../social/interfaces/social.interface';
+import { enrollType } from '../users/entities/user.entity';
 
 @ApiTags('Auth')
 @Controller({
@@ -29,18 +31,23 @@ export class AuthNaverController {
   @Get('register')
   @UseGuards(AuthGuard('naver'))
   @HttpCode(HttpStatus.CREATED)
-  @ApiResponse({ status: 200, description: '회원가입 성공' })
-  @ApiResponse({ status: 409, description: '이미 존재하는 이메일' })
-  register(@Body() loginDto: AuthNaverLoginDto) {
-    const socialData = this.authNaverService.getProfileByToken(loginDto);
+  register() {
+    // const socialData = this.authNaverService.getProfileByToken(loginDto);
     // return this.authService.validateSocialLogin('google', socialData);
   }
 
   @Get('callback')
   @UseGuards(AuthGuard('naver'))
   async callback(@Req() req, @Res() res) {
-    // const socialData = await this.authGoogleService.getProfileByToken(loginDto);
+    // console.log(req.user);
+    const socialData: SocialInterface = {
+      enroll_type: enrollType.naver,
+      email: req.user.email,
+      password: null,
+    };
+    console.log(socialData);
 
+    await this.authService.validateSocialLogin('naver', socialData);
     res.redirect('http://localhost:4000');
   }
 }
