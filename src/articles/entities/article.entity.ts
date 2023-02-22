83fac9886c5f3e0b6e0f8293b588c1be02780c00
synodical <lena0323park@naver.com>
2023-02-22 21:33:19 +0900

    FEAT : Create article.entity.ts

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,10 +1,29 @@
 import {
+  Column,
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
+  PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import { EntityHelper } from 'src/utils/entity-helper';
 
 @Entity()
-export class Article extends EntityHelper {}
+export class Article extends EntityHelper {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({ nullable: false })
+  writer_id: number;
+
+  @Column({ nullable: false, default: 0 })
+  liked: number;
+  @Column({ nullable: false })
+  title: string;
+  @Column({ nullable: false })
+  content: string;
+  @CreateDateColumn()
+  created_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
