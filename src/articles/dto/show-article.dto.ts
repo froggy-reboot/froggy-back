f203c2cf24e81f7efbe02a4fc5448530a37b9baa
이예은 <yeeun_1ee@naver.com>
2023-02-28 13:57:50 +0900

    FEAT : Create show-article.dto.ts

---
+++ src/articles/dto/show-article.dto.ts
@@ -0,0 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+
+export class ShowArticleDto {
+  @ApiProperty({ example: '1' })
+  @IsNotEmpty()
+  @IsNumber()
+  writer_id: number;
+
+  @ApiProperty({ example: '0' })
+  @IsNotEmpty()
+  liked: number;
+
+  @ApiProperty({
+    example: 'synodic',
+  })
+  @IsNotEmpty()
+  @IsString()
+  nickname: string;
+
+  @ApiProperty({
+    example: '제목입니다. 안녕하세요 저는 뜨개질을 하기 어려워해요',
+  })
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty({ example: '본문입니다. 어떡하죠, 도와주세요.' })
+  @IsNotEmpty()
+  content: string;
+  @ApiProperty({ example: 3 })
+  @IsNotEmpty()
+  comments: number;
+  @ApiProperty({ example: '2023-02-27T15:15:49.695Z' })
+  @IsDate()
+  created_at: Date;
+}
