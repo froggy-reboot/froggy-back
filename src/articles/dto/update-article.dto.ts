f2a13c598b45dba0bc7b872deea6ee961155ee7c
synodical <lena0323park@naver.com>
2023-03-19 11:18:25 +0900

    FEAT : update article

--- src/articles/dto/update-article.dto.ts
+++ src/articles/dto/update-article.dto.ts
@@ -1,12 +1,17 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateArticleDto } from './create-article.dto';
 import { IsNotEmpty, IsOptional } from 'class-validator';
+import { articleType } from '../entities/article.entity';
 
 export class UpdateArticleDto extends PartialType(CreateArticleDto) {
   @ApiProperty({ example: '1' })
-  @IsOptional()
   @IsNotEmpty()
   writer_id: number;
+
+  @ApiProperty({ example: '자유' })
+  @IsOptional()
+  @IsNotEmpty()
+  article_type: articleType;
 
   @ApiProperty({ example: '0' })
   @IsOptional()
@@ -14,13 +19,13 @@ export class UpdateArticleDto extends PartialType(CreateArticleDto) {
   liked: number;
 
   @ApiProperty({
-    example: '변경된 제목입니다.',
+    example: '제목입니다. 안녕하세요 저는 뜨개질을 하기 어려워해요',
   })
   @IsOptional()
   @IsNotEmpty()
   title: string;
 
-  @ApiProperty({ example: '변경된 본문입니다.' })
+  @ApiProperty({ example: '본문입니다. 어떡하죠, 도와주세요.' })
   @IsOptional()
   @IsNotEmpty()
   content: string;
