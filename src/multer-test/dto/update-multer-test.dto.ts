6b8c52f4ee6e3998d282fa0b64a77c033a37f863
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2023-05-07 17:44:29 +0900

    delete : delete multer test

--- src/multer-test/dto/update-multer-test.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMulterTestDto } from './create-multer-test.dto';
-
-export class UpdateMulterTestDto extends PartialType(CreateMulterTestDto) {}
