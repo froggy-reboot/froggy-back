3b4b36a92e8bad7c4be077ea33da5dc5843f2a64
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2023-05-27 16:55:12 +0900

    Feat : add report module and service

---
+++ src/report/entities/reprot.entity.ts
@@ -0,0 +1,39 @@
+import { reportType } from 'src/utils/common/custom.enum';
+import { EntityHelper } from 'src/utils/entity-helper';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Report extends EntityHelper {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  reporterId: number;
+
+  @Column({ nullable: false })
+  type: reportType;
+
+  @Column({ nullable: false })
+  targetId: number;
+
+  @Column({ nullable: true })
+  title: string;
+
+  @Column({ nullable: false, length: 1000 })
+  content: string;
+
+  @Column({ nullable: false })
+  reason: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
