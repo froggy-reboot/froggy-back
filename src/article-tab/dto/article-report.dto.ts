75947cf0baaadac02c7806529a52e92713f39dca
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2023-05-27 20:21:41 +0900

    Fix : report reason optional2

--- src/article-tab/dto/article-report.dto.ts
+++ src/article-tab/dto/article-report.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class ReportArticleDto {
   @ApiProperty({ example: '1' })
